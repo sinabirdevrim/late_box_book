9f546a5f371b3f6620da7d5c24019c4d46c57f98
Sina Birdevrim <>
2020-02-07 19:56:06 +0300

    change push type

--- lib/common/notification_handler.dart
+++ lib/common/notification_handler.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
@@ -7,76 +6,72 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
     FlutterLocalNotificationsPlugin();
 
+Future<void> onBackgroundMessageHandler(Map<String, dynamic> message) {
+  if (message.containsKey('data')) {
+   // NotificationHandler.showNotification(message);
+  }
+
+  return Future<void>.value();
+}
+
 class NotificationHandler {
   FirebaseMessaging _fcm = FirebaseMessaging();
 
   static final NotificationHandler _singleton = NotificationHandler._internal();
-
   factory NotificationHandler() {
     return _singleton;
   }
 
   NotificationHandler._internal();
-
   BuildContext myContext;
 
   initializeFCMNotification(BuildContext context) async {
     myContext = context;
 
     var initializationSettingsAndroid =
-    AndroidInitializationSettings('app_icon');
+        AndroidInitializationSettings('app_icon');
     var initializationSettingsIOS = IOSInitializationSettings(
         onDidReceiveLocalNotification: onDidReceiveLocalNotification);
     var initializationSettings = InitializationSettings(
         initializationSettingsAndroid, initializationSettingsIOS);
-    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+    await flutterLocalNotificationsPlugin.initialize(initializationSettings,
         onSelectNotification: onSelectNotification);
-
 
     _fcm.configure(
       onMessage: (Map<String, dynamic> message) async {
-        showNotification(message);
+        //showNotification(message);
       },
-      onBackgroundMessage: myBackgroundMessageHandler,
+      onBackgroundMessage: onBackgroundMessageHandler,
       onLaunch: (Map<String, dynamic> message) async {},
       onResume: (Map<String, dynamic> message) async {},
     );
   }
 
-  Future onSelectNotification(String payload) async {
-
-  }
+  Future onSelectNotification(String payload) async {}
 
   Future onDidReceiveLocalNotification(
       int id, String title, String body, String payload) {}
 
-
-  static Future<void> myBackgroundMessageHandler(Map<String, dynamic> message) {
-   // showNotification(message);
-    return Future<void>.value();
-  }
-
   getUserToken(Function(String) token) {
-    _fcm.getToken().then((newToken){
+    _fcm.getToken().then((newToken) {
+      debugPrint("Token: "+ newToken);
       token(newToken);
     });
   }
 
   static void showNotification(Map<String, dynamic> message) async {
-
     String title = message["data"]["title"];
     String msg = message["data"]["message"];
 
     var mMessage = Person(
         name: title,
-        key: '1',
-        //icon: userURLPath,
-        iconSource: IconSource.FilePath);
+        key: '1');
+
     var messageStyle = MessagingStyleInformation(mMessage,
         messages: [Message(msg, DateTime.now(), mMessage)]);
 
     var androidPlatformChannelSpecifics = AndroidNotificationDetails(
-        '1', 'Genaral', 'Late Box App Notf.',
+        '12', 'Genaral', 'Late Box App Notf.',
         style: AndroidNotificationStyle.Messaging,
         styleInformation: messageStyle,
         importance: Importance.Max,
@@ -86,8 +81,10 @@ class NotificationHandler {
     var platformChannelSpecifics = NotificationDetails(
         androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
 
+
     await flutterLocalNotificationsPlugin.show(
         0, title, msg, platformChannelSpecifics,
         payload: jsonEncode(message));
   }
+
 }
--- lib/screens/home/home_scree.dart
+++ lib/screens/home/home_scree.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:late_box_book/blocs/user/bloc.dart';
